2eca4e2ab491fe95630a2f920265a2063ec2b9cc
Vargas_Veronica <vargas.veronica55@gmail.com>
2022-05-13 16:01:48 -0300

    footer

--- Vargas.Veronica/src/app/app.module.ts
+++ Vargas.Veronica/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { HysComponent } from './components/hys/hys.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HysComponent
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
