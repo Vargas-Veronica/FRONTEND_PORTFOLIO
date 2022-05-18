a37a69dfb205a375a945d3a1fcca43406bf5cd61
Vargas_Veronica <vargas.veronica55@gmail.com>
2022-05-17 23:02:56 -0300

    java

--- Vargas.Veronica/BACKEND/portfolio/src/main/java/com/vargasveronica/portfolio/Service/ImpPersonaService.java
+++ Vargas.Veronica/BACKEND/portfolio/src/main/java/com/vargasveronica/portfolio/Service/ImpPersonaService.java
@@ -5,10 +5,41 @@
  */
 package com.vargasveronica.portfolio.Service;
 
+import com.vargasveronica.portfolio.Entity.Persona;
+import com.vargasveronica.portfolio.Interface.IPersonaService;
+import com.vargasveronica.portfolio.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author varga
  */
-public class ImpPersonaService {
+@Service
+public class ImpPersonaService implements IPersonaService{
+    @Autowired IPersonaRepository ipersonaRepository;
+
+    @Override
+    public List<Persona> getPersona() {
+       List <Persona> persona = ipersonaRepository.findAll();
+       return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id).orElse(null);
+        return persona ;
+    }
     
 }
