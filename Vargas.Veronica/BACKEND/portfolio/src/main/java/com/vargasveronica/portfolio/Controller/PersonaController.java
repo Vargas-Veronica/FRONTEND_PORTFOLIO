4795a2679c68e4c2bc33735f7867728a0f703b59
Vargas_Veronica <vargas.veronica55@gmail.com>
2022-05-18 22:29:09 -0300

    primer prueba implementacion con FRONTEND

--- Vargas.Veronica/BACKEND/portfolio/src/main/java/com/vargasveronica/portfolio/Controller/PersonaController.java
+++ Vargas.Veronica/BACKEND/portfolio/src/main/java/com/vargasveronica/portfolio/Controller/PersonaController.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.vargasveronica.portfolio.Controller;
 
 import com.vargasveronica.portfolio.Entity.Persona;
 import com.vargasveronica.portfolio.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,15 +15,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author varga
- */
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService ;
     
-    @GetMapping ("personas/traer")
+    @GetMapping ("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
